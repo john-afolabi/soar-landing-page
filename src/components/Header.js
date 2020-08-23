b281b70c2bbdb621969f9b36ef9d267d9e94d4dd
John Afolabi <johnafolabi.da@gmail.com>
2020-08-23 01:08:17 +0100

    Build and style Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,8 @@
 import logo from "../img/svg/logo.svg";
+import google from "../img/svg/brands/google.svg";
+import verge from "../img/svg/brands/verge.svg";
+import slack from "../img/svg/brands/slack.svg";
+import boost from "../img/svg/to-the-moon.svg";
 
 export const Header = (open) => {
 	const template = `
@@ -10,7 +14,6 @@ export const Header = (open) => {
 					<li><a href="#schedule">Schedule Strategy Call</a></li>
 					<li><a href="#blog">Blog</a></li>
 				</ul>
-
 				<button class="nav-cta">Sign Up</button>
 			</nav>
 			<button class="hamburger">menu</button>
@@ -22,47 +25,72 @@ export const Header = (open) => {
 					<img src="${logo}" class="brand-logo" />
 
 					<ul>
-						<li class="nav__list">
+						<li class="nav-list">
 							<a href="#about" class="nav-link">About</a>
 						</li>
-						<li class="nav__list">
+						<li class="nav-list">
 							<a href="#packages" class="nav-link"
 								>Growth Packages</a
 							>
 						</li>
-						<li class="nav__list">
+						<li class="nav-list">
 							<a href="#schedule" class="nav-link"
 								>Schedule Strategy Call</a
 							>
 						</li>
-						<li class="nav__list">
+						<li class="nav-list">
 							<a href="#blog" class="nav-link">Blog</a>
 						</li>
 					</ul>
 
-					<button class="nav-cta">Sign Up</button>
+					<button class="btn-primary">Sign Up</button>
 				</nav>
 
-				<div class="hero-text">
-					Grow with world-class Reddit marketing services
-					<p>
-						Achieve your marketing goals on Reddit. Go viral,
-						improve your reputation & more
-					</p>
-					<button>Get Started</button>
-				</div>
+				<div class="hero-bg">
+					<div class="hero">
+						<div class="hero-text">
+							<h2>
+								Grow with world-class Reddit marketing services
+							</h2>
+							<p>
+								Achieve your marketing goals on Reddit. Go
+								viral, improve your reputation & more
+							</p>
+							<button class="btn-primary">Get Started</button>
 
-				<div class="brands">
-					<img src="" alt="verge" />
-					<img src="" alt="slack" />
-					<img src="" alt="google" />
+							<div class="brands">
+								<img src="${verge}" alt="verge" />
+								<img src="${slack}" alt="slack" />
+								<img src="${google}" alt="google" />
+							</div>
+							<div class="summary-text">
+								<span>Start for free</span>
+								<span>Add users for $10/month</span>
+								<span
+									>Add Powerful Features for less than
+									$10/month</span
+								>
+							</div>
+						</div>
+						<img
+							src="${boost}"
+							alt="reddit boost"
+							class="hero-image"
+						/>
+					</div>
 				</div>
-				<div class="summary-text">
-					<span>Start for free</span>
-					<span>Add users for $10/month</span>
-					<span>Add Powerful Features for less than $10/month</span>
-				</div>
-				<img src="" alt="reddit boost" class="hero-image" />
+				<svg
+					id="bottom-curve"
+					width="100%"
+					height="100"
+					viewBox="0 0 1331 206"
+					xmlns="http://www.w3.org/2000/svg"
+					preserveAspectRatio="none"
+				>
+					<path
+						d="M0 0H1331L155.647 203.104C74.3446 217.153 0 154.554 0 72.0462V0Z"
+					/>
+				</svg>
 			</div>
 		</header>
     `;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { Header } from "./components/Header.js";
 
+import "./scss/main.scss";
+
 const body = document.querySelector(".container");
 
 const app = () => {
@@ -9,3 +11,24 @@ const app = () => {
 };
 
 app();
+
+const mobileNav = document.querySelector(".mobile-nav");
+console.log(mobileNav.classList);
+const openNav = () => {
+	if (mobileNav.classList.contains("openNav")) {
+		mobileNav.classList.remove("openNav");
+		document.querySelector(".hamburger").textContent = "menu";
+	} else {
+		mobileNav.classList.add("openNav");
+		document.querySelector(".hamburger").textContent = "close";
+	}
+};
+document.querySelector(".hamburger").onclick = openNav;
+
+const allLinks = document.querySelectorAll(".links-nav li a");
+allLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		mobileNav.classList.remove("openNav");
+		document.querySelector(".hamburger").textContent = "menu";
+	});
+});
